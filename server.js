d154985ff857cbb79b00c4cb76dd91435e45c5d9
Hope Ngerebara <hope.ngerebara@andela.com>
2017-05-03 18:09:28 +0100

    update the script for starting this app

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.static('src'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'app/index.html'));
+  res.sendFile(path.resolve(__dirname, 'app', 'index.html'));
 });
 app.listen(port, () => {
   console.log(`Express server is up on port ${port}`);
