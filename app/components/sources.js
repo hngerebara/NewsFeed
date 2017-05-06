3abdfa8777445f015cb48b7c0afc59f09aad0b39
Hope Ngerebara <hope.ngerebara@andela.com>
2017-05-06 16:48:35 +0100

    fixed search issue

--- app/components/sources.js
+++ app/components/sources.js
@@ -42,9 +42,11 @@ export default class Sources extends React.Component {
     }
 
     OnFilterChange(event) {
-        const allSources = this.state.sources[0];
+        const allSources = SourceStore.getAll()[0];
         const filterBy = event.target.value.toString().toLowerCase();
         const filteredList = [];
+         console.log(allSources,"all sources")
+         console.log(event.target.value,"value")
         if (!event.target.value) {
             this.setState({
                 sources: allSources
@@ -54,12 +56,14 @@ export default class Sources extends React.Component {
                 let value = allSources[index].name;
                 if ((new RegExp(filterBy)).test(value.toLowerCase())) {
                     filteredList.push(allSources[index]);
+                    console.log(filteredList,"filetrred list")
                     this.setState({
                         sources: [filteredList]
                     })
                 }
             }
         }
+    
 
     }
     render() {
@@ -69,7 +73,7 @@ export default class Sources extends React.Component {
                 <div id="sidebar-wrapper" className="col-lg-2 list-group">
                     <h5>Please select News Source </h5>
                     <input type="text" className="form-control" placeholder="Search News...."
-                         onKeyUp={this.OnFilterChange.bind(this)} />
+                         onChange={this.OnFilterChange} />
                     <div onLoad={this._onLoad}>
                         { allSources && allSources.map((item, index) => <SourceItem key={index} value={item.id} name={item.name}
                           onclick={() => this._onChange(item.id, item.sortBysAvailable)} />)
